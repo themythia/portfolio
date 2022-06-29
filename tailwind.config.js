855d75f34e7e87acb1f655642ac74786c59d546c
Emir Al <mythia@windowslive.com>
2022-06-29 12:43:06 +0300

    feat: configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      sm: '600px',
+      md: '905px',
+      lg: '1240px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        josefinSans: ['Josefin Sans'],
+      },
+      colors: {
+        dark: '#12263A',
+        dark800: '#243A52',
+        light: '#F4EDEA',
+        accent: '#06BBC1',
+      },
+    },
   },
   plugins: [],
-}
+};
