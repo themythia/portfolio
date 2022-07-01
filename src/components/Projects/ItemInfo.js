57e5c2b5a487032ffd8c704967a546ad31454c76
Emir Al <mythia@windowslive.com>
2022-07-01 12:03:32 +0300

    refactor: remove underline from project titles

--- src/components/Projects/ItemInfo.js
+++ src/components/Projects/ItemInfo.js
@@ -45,7 +45,7 @@ const ItemInfo = ({ project }) => {
   return (
     <div className='h-auto w-full max-w-lg flex flex-col justify-center mt-4 bg-light100/60 dark:bg-dark800/60 rounded-lg mb-4 p-4 sm:p-8'>
       <div className='flex justify-between items-center mb-4'>
-        <h3 className='font-josefinSans font-semibold text-xl sm:text-2xl text-left underline underline-offset-2 decoration-accent decoration-4'>
+        <h3 className='font-josefinSans font-semibold text-xl sm:text-2xl text-left'>
           {text[project].title}
         </h3>
         <div className='flex items-center last:mr-0'>
