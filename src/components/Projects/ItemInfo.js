1c13d12062cd70a737c169d4d79272832e0783b7
Emir Al <mythia@windowslive.com>
2022-07-01 13:43:55 +0300

    refactor: move project info text object into utils folder

--- src/components/Projects/ItemInfo.js
+++ src/components/Projects/ItemInfo.js
@@ -1,3 +1,4 @@
+import projectInfo from '../../utils/projectInfo';
 import Icon from '../shared/Icon';
 
 const Link = ({ link, icon }) => {
@@ -14,43 +15,20 @@ const Link = ({ link, icon }) => {
 };
 
 const ItemInfo = ({ project }) => {
-  const text = {
-    f1ref: {
-      title: 'F1/REF',
-      desc: 'A personal project about Formula 1. Check news, race results, team & driver stats and race schedule.',
-      stack: ['React, Redux Toolkit, TailwindCSS, Ergast API, Vercel'],
-      demo: 'https://f1ref-v2.vercel.app/',
-      github: 'https://github.com/themythia/f1ref-v2',
-    },
-    resonance: {
-      title: 'Resonance',
-      desc: 'A music streaming app that provides 30 second previews from Spotify Web API. Please check GitHub repository for demo account.',
-      stack: ['React, styled-components, Spotify Web API, Vercel'],
-      demo: 'https://resonance-zeta.vercel.app/',
-      github: 'https://github.com/themythia/resonance',
-    },
-    trello: {
-      title: 'Trello Clone',
-      desc: 'Front-end clone of Trello built with React, Redux & React Beautiful DnD.',
-      stack: ['React, Redux, react-beautiful-dnd, SASS, Vercel'],
-      demo: 'https://trello-clone-delta-fawn.vercel.app/',
-      github: 'https://github.com/themythia/trello-clone',
-    },
-  };
   return (
     <div className='h-auto w-full max-w-lg flex flex-col justify-center mt-4 bg-light100/60 dark:bg-dark800/60 rounded-lg mb-4 p-4 sm:p-8'>
       <div className='flex justify-between items-center mb-4'>
         <h3 className='font-josefinSans font-semibold text-xl sm:text-2xl text-left'>
-          {text[project].title}
+          {projectInfo[project].title}
         </h3>
         <div className='flex items-center last:mr-0'>
-          <Link link={text[project].github} icon='github' />
-          <Link link={text[project].demo} icon='link' />
+          <Link link={projectInfo[project].github} icon='github' />
+          <Link link={projectInfo[project].demo} icon='link' />
         </div>
       </div>
-      <p className='md:text-lg'>{text[project].desc}</p>
+      <p className='md:text-lg'>{projectInfo[project].desc}</p>
       <p className='text-dark/80 dark:text-light/60 text-sm mt-4'>
-        {text[project].stack}
+        {projectInfo[project].stack}
       </p>
     </div>
   );
---
+++ src/utils/projectInfo.js
@@ -0,0 +1,24 @@
+const projectInfo = {
+  f1ref: {
+    title: 'F1/REF',
+    desc: 'A personal project about Formula 1. Check news, race results, team & driver stats and race schedule.',
+    stack: ['React, Redux Toolkit, TailwindCSS, Ergast API, Vercel'],
+    demo: 'https://f1ref-v2.vercel.app/',
+    github: 'https://github.com/themythia/f1ref-v2',
+  },
+  resonance: {
+    title: 'Resonance',
+    desc: 'A music streaming app that provides 30 second previews from Spotify Web API. Please check GitHub repository for demo account.',
+    stack: ['React, styled-components, Spotify Web API, Vercel'],
+    demo: 'https://resonance-zeta.vercel.app/',
+    github: 'https://github.com/themythia/resonance',
+  },
+  trello: {
+    title: 'Trello Clone',
+    desc: 'Front-end clone of Trello built with React, Redux & React Beautiful DnD.',
+    stack: ['React, Redux, react-beautiful-dnd, SASS, Vercel'],
+    demo: 'https://trello-clone-delta-fawn.vercel.app/',
+    github: 'https://github.com/themythia/trello-clone',
+  },
+};
+export default projectInfo;
